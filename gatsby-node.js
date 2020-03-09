0f24245147cd78a7c73bad6b10ae71104bdab474
Jussi Kinnula <jussi.kinnula@gmail.com>
2020-03-09 21:54:59 +0200

    Use file-loader for local assets

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,26 +1,19 @@
-const testModulePath = modulePath => {
-  return /node_modules/.test(modulePath) &&
-    !/node_modules\/(gatsby-source-prismic-graphql|gatsby-source-graphql-universal|ts-optchain)/.test(modulePath);
-};
-
 exports.createPages = require('./src/utils/createPages');
 
 exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
   const config = getConfig();
-
-  const fontsRule = {
-    test: /\.(woff|woff2|otf|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-    loader: 'file-loader',
-    options: {
-      name: 'static/fonts/[name].[ext]'
-    }
-  };
-
-  const rules = config.module.rules.map(rule => {
+    
+  config.module.rules = config.module.rules.map(rule => {
     // Exclude all node_modules from transpilation,
     // except for 'gatsby-source-prismic-graphql', 'gatsby-source-graphql-universal' and 'ts-optchain'
     if (String(rule.test) === String(/\.(js|mjs|jsx)$/)) {
-      return { ...rule, exclude: testModulePath };
+      return {
+        ...rule,
+        exclude: modulePath => {
+          return /node_modules/.test(modulePath) &&
+            !/node_modules\/(gatsby-source-prismic-graphql|gatsby-source-graphql-universal|ts-optchain)/.test(modulePath);
+        }
+     };
     }
 
     // Force images not to be base64 encoded
@@ -31,6 +24,5 @@ exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
     return rule;
   });
 
-  config.module.rules = [fontsRule, ...rules];
   actions.replaceWebpackConfig(config);
 };
